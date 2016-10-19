29393deb30603705352e436d18c0754fd0e38307
Yulong Niu <niuylscu@gmail.com>
2016-10-20 03:06:33 +0800

    add STRING bit score batch

---
+++ STRING_homo.R
@@ -0,0 +1,10 @@
+library('STRINGdb')
+
+sp <- get_STRING_species(version="10", species_name=NULL)
+
+## human
+string_db <- STRINGdb$new(version="10", species=9606, score_threshold=0, input_directory="" )
+pList <- string_db$get_proteins()
+
+string_db$get_homologs(pList[1, 1], target_species_id=10090)
+sapply(1:10, function(x)return(string_db$get_homologs(pList[1, 1], target_species_id=sp[x, 1])))
