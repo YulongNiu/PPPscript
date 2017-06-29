112ee4272d653c7232887b8556554259e70785c1
Yulong Niu <niuylscu@gmail.com>
2017-06-30 05:10:49 +0800

    add top test

--- script_MMM.R
+++ script_MMM.R
@@ -84,15 +84,114 @@ save(MMMLinksWhole, MMMAnnoEntrez, file = 'MMM12.RData')
 #################################################################
 
 ################select all present#####################
+library('doParallel')
+library('foreach')
+
 setwd('/home/Yulong/RESEARCH/neuro/Bioinfor/PhyloViz/phyloMito/wholenetwork0001/MMM/')
 load('/home/Yulong/RESEARCH/neuro/Bioinfor/PhyloViz/phyloMito/wholenetwork0001/wholePhyloData.RData')
 load('/home/Yulong/RESEARCH/neuro/Bioinfor/PhyloViz/phyloMito/wholenetwork0001/MMM/MMM12.RData')
 
-## set threshold 95%
-thres <- 0.95
+## set threshold 90%
+thres <- 0.9
 phyloP <- wholePhyloDataNet[rowSums(wholePhyloDataNet) >= floor(ncol(wholePhyloDataNet) * thres), , drop = FALSE]
 
 ## check MMM12
 MMMAnnoEntrez[, 3] <- paste0('hsa:', MMMAnnoEntrez[, 3])
-sum(rownames(phyloP) %in% MMMAnnoEntrez[, 3])
+entrezLinks <- cbind(entrez1 = MMMAnnoEntrez[match(MMMLinksWhole[, 1], MMMAnnoEntrez[, 1]), 3],
+                     entrez2 = MMMAnnoEntrez[match(MMMLinksWhole[, 2], MMMAnnoEntrez[, 1]), 3])
+MMMLinksWhole <- cbind(MMMLinksWhole[, 1:4], entrezLinks, mmm = MMMLinksWhole[, 5])
+
+## select thres links
+thresLinks <- MMMLinksWhole[(MMMLinksWhole[, 5] %in% rownames(phyloP)) & (MMMLinksWhole[, 6] %in% rownames(phyloP)), , drop = FALSE]
+thresPer <- cbind(p1 = rowSums(phyloP[match(thresLinks[, 5], rownames(phyloP)), ])/ncol(wholePhyloDataNet),
+                  p2 = rowSums(phyloP[match(thresLinks[, 6], rownames(phyloP)), ])/ncol(wholePhyloDataNet))
+rownames(thresPer) <- NULL
+thresPer <- apply(thresPer, 1:2, function(x) {
+  return(paste0(round(x*100, 1), '%'))
+})
+thresLinks <- cbind(thresLinks[, 1:6],
+                    thresPer,
+                    mmm = thresLinks[, 7])
+
+##~~~~~~~~~~~~~~~~~~~~~~~~top number~~~~~~~~~~~~~~~~~~~~~~~~~~~
+GetPosJac <- function(jacMat, corMat, linkVec, corSet){
+  # USE: choose the top position in Jaccard mat
+  # INPUT: 'jacMat' Jaccard matrix. 'corMat' correlation matrix. 'jacMat' and 'corMat' should have the same rownames and colnames. 'linkVec' linkages (from, to, jaccard, cor), for example c("hsa:55851" "hsa:10569" "0.389313"  "0.5293421"). 'corSet' is the correlation cutoff.
+  # OUTPUT: the minimum top number in 'jacMat'
+  # EXEAMPLE: topInter(jaccardSim, wholeCor, c('hsa:1004', 'hsa:34235'), 0)
+
+  geneNames <- rownames(jacMat)
+  linkFromNum <- match(linkVec[1], geneNames)
+  linkToNum <- match(linkVec[2], geneNames)
+  jacValue <- as.numeric(linkVec[3])
+  corValue <- as.numeric(linkVec[4])
+
+  ## select with corSet
+  if (corValue <= corSet) {
+    ## the last one
+    minTop <- length(geneNames)
+  } else {
+    jacFrom <- jacMat[, linkFromNum]
+    corFrom <- corMat[, linkFromNum]
+    jacFrom <- jacFrom[corFrom > corSet]
+
+    jacTo <- jacMat[, linkToNum]
+    corTo <- corMat[, linkToNum]
+    jacTo <- jacTo[corFrom > corSet]
+
+    fromTop <- sum(jacFrom >= jacValue)
+    toTop <- sum(jacTo >= jacValue)
+    minTop <- min(fromTop, toTop)
+  }
+
+   return(minTop)
+}
+
+
+GetPosJacBatch <- function(jacMatDesc, corMatDesc, linkMat, corSet, n = 1) {
+
+  require('bigmemory')
+  require('doParallel')
+  require('foreach')
+
+  jacMat <- attach.big.matrix(jacMatDesc)
+  corMat <- attach.big.matrix(corMatDesc)
+
+  ## register multiple core
+  registerDoParallel(cores = n)
+  linkNum <- nrow(linkMat)
+
+  batchVec <- foreach(i = 1:linkNum, .combine = c) %dopar% {
+    print(paste0('It is running ', i, ' in a total of ', linkNum, '.'))
+    eachSD <- GetPosJac(jacMat, corMat, linkMat[i, ], corSet)
+    return(eachSD)
+  }
+
+  ## stop multiple core
+  stopImplicitCluster()
+
+  return(batchVec)
+}
+
+SimJaccardR <- function(pairProfile) {
+  f <- pairProfile[, 1]
+  t <- pairProfile[, 2]
+
+  A <- sum((f + 2*t) == 3)
+
+  jac <- A / (sum(f) + sum(t) - A)
+  return(jac)
+}
+
+linkMat <- thresLinks[, 5:6]
+linkMat <- apply(linkMat, 1:2, as.character)
+linkjc <- foreach(i = seq_len(nrow(linkMat)), .combine = rbind) %do% {
+  eachP <- t(phyloP[match(linkMat[i, ], rownames(phyloP)), ])
+  return(c(SimJaccardR(eachP),
+           cor(eachP)[1, 2]))
+}
+linkMat <- cbind(linkMat, linkjc)
+
+distTop <- GetPosJacBatch('../jaccardSim.desc', '../wholeCor.desc', linkMat, 0, n = 4)
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #######################################################
